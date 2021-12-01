9d3d1215c7ec68c930c38f84283158ce8a78abd1
kevinmcm1229 <kevinmcm1229@gmail.com>
2021-12-01 09:49:10 -0700

    Added conversation list on profile page. Added links from these list items to conversation show page

--- shine-frontend/src/pages/Profile.js
+++ shine-frontend/src/pages/Profile.js
@@ -2,7 +2,7 @@ import {useState, useEffect} from 'react';
 import authorizationModel from '../models/authorization';
 // import useAccount from "../hooks/useAccount";
 // import {Params} from "../config/routing";
-import {useParams} from "react-router-dom";
+import {BrowserRouter as Router, useParams, Link} from "react-router-dom";
 
 
 const Profile = () => {
@@ -26,28 +26,35 @@ const Profile = () => {
     }
 
     function generateList(input) {
-        console.log(input);
-        return input.map((conversation, index) => (
-            <h3>{conversation.name}</h3>
-        ));
+        if (input) {
+            const objectToArray = Object.values(input);
+
+            return input.map((conversation, index) => (
+                <Link to={`/conversations/${conversation._id}`}>
+                    <h3>{conversation.name}</h3>
+                </Link>
+                //Link to conversation show @ {conversation._id}
+                // <Link to=`/conversations/${conversation._id}`>Test </Link>
+            ));
+        }
     }
 
     const conversationArray = user.conversation;
     console.log(conversationArray)
     console.log(typeof conversationArray)
     // const objectToArray = Object.values(conversationArray);
-    // if (conversationArray) {
-    //     const objectToArray = Object.values(conversationArray);
-    //     console.log(objectToArray)
-    //     console.log(objectToArray[0].name)
-    // }
+    if (conversationArray) {
+        const objectToArray = Object.values(conversationArray);
+        console.log(objectToArray)
+        console.log(objectToArray[0].name)
+    }
 
     return (
     <>
         <div>
             <h1>Welcome to {user.username} Account!</h1>
             <h4>Name: {user.name}</h4>
-            {/* {generateList(objectToArray)} */}
+            {generateList(conversationArray)}
         </div>
     </>
     );
