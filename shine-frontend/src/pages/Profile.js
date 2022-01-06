4472212567489e1781088e5a4d846dd6b23dcf47
kevinmcm1229 <kevinmcm1229@gmail.com>
2022-01-05 20:21:58 -0700

    Added to Profile.js to display all usernames. Cleaned code.

--- shine-frontend/src/pages/Profile.js
+++ shine-frontend/src/pages/Profile.js
@@ -1,16 +1,13 @@
 import {useState, useEffect} from 'react';
 import authorizationModel from '../models/authorization';
-// import useAccount from "../hooks/useAccount";
-// import {Params} from "../config/routing";
 import {BrowserRouter as Router, useParams, Link} from "react-router-dom";
-
 
 const Profile = () => {
     const [user, setUser] = useState("");
-    // const {id} = useParams();
-    // console.log(id);
-    // console.log({id});
-    console.log(user)
+    const [users, setUsers] = useState("");
+
+    // console.log(user);
+    // console.log(users);
 
     useEffect(function() {
         console.log("I'm using Effect")
@@ -19,34 +16,29 @@ const Profile = () => {
 
     function fetchUser() {
         authorizationModel.profile().then((data) => {
-            // console.log(data);
-            // console.log(id);
             setUser(data.user);
+            setUsers(data.users);
         })
     }
 
+    const conversationArray = user.conversation;
+
     function generateList(input) {
         if (input) {
-            const objectToArray = Object.values(input);
-
             return input.map((conversation, index) => (
                 <Link to={`/conversations/${conversation._id}`}>
                     <h3>{conversation.name}</h3>
                 </Link>
-                //Link to conversation show @ {conversation._id}
-                // <Link to=`/conversations/${conversation._id}`>Test </Link>
             ));
         }
     }
-
-    const conversationArray = user.conversation;
-    console.log(conversationArray)
-    console.log(typeof conversationArray)
-    // const objectToArray = Object.values(conversationArray);
-    if (conversationArray) {
-        const objectToArray = Object.values(conversationArray);
-        // console.log(objectToArray)
-        // console.log(objectToArray[0].name)
+    
+    function generateUserList(input) {
+        if (input) {
+            return input.map((profile, index) => (
+                <li>{profile.username}</li>
+            ));
+        }
     }
 
     return (
@@ -55,6 +47,10 @@ const Profile = () => {
             <h1>Welcome to {user.username} Account!</h1>
             <h4>Name: {user.name}</h4>
             {generateList(conversationArray)}
+            <h2>All Users:</h2>
+            <ul>
+                {generateUserList(users)}
+            </ul>
         </div>
     </>
     );
