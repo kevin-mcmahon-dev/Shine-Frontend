778ab50807f2be33e8dd38d170d8e1d7cd805389
kevinmcm1229 <kevinmcm1229@gmail.com>
2022-01-14 01:24:07 -0700

    Removed comments and console.logs

--- shine-frontend/src/pages/ConversationShow.js
+++ shine-frontend/src/pages/ConversationShow.js
@@ -1,21 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import DataModel from '../models/conversations';
 import authorizationModel from '../models/authorization';
-// import useAccount from "../hooks/useAccount";
-// import {Params} from "../config/routing";
 import {useParams, useNavigate} from "react-router-dom";
 import "./ConversationShow.css"
 
 const ConversationShow = () => {
     const {id} = useParams();
-    // console.log(id);
     const [conversation, setConversation] = useState([]);
     const [user, setUser] = useState("");
     const [content, setContent] = useState({});
-    // content: ""
+
     const nav = useNavigate();
-    // console.log({id});
-    console.log(id)
+    const userArray = conversation.user
 
     useEffect(function() {
         console.log("I'm using Effect")
@@ -32,33 +28,26 @@ const ConversationShow = () => {
 
     function fetchConversation() {
         DataModel.conversationShow(id).then((data) => {
-            // console.log(data);
-            // console.log(id);
             console.log(data.conversation);
             setConversation(data.conversation);
         })
     }
+
     function fetchContent() {
         setContent("Enter Message")
     }
 
     function fetchUser() {
         authorizationModel.profile().then((data) => {
-            // console.log(data);
-            // console.log(id);
             setUser(data.user);
         })
     }
-    // console.log(JSON.stringify(conversation, null, 2));
-    const userArray = conversation.user
 
     function generateUserList(input) {
         if (input) {
-            // const objectToArray = Object.values(input);
-
             return input.map((profile, index) => (
                 <>
-                {profile._id !== user._id ? <span>{profile.username}</span> : null}
+                    {profile._id !== user._id ? <span>{profile.username}</span> : null}
                 </>
             ));
         }
@@ -100,25 +89,13 @@ const ConversationShow = () => {
         }
     }
 
-    console.log(content);
-    console.log(user._id);
     function handleSubmit(event) {
         event.preventDefault();
 
-        // (id, {content: "hello"})
         DataModel.createMessage(id, content).then((response) => {
-            console.log(response);
-            console.log(content);
-            // localStorage.setItem("uid", response.signedJwt);
             fetchConversation()
             fetchUser()
             fetchContent()
-            // if (response.status === 200) {
-            //     console.log("Wow");
-            //     fetchConversation()
-            //     fetchUser()
-            // }
-            // generateMessageList(conversation.messages)
         })
     }
 
@@ -134,21 +111,13 @@ const ConversationShow = () => {
         <>
             <div>
                 <h1>Conversation with {generateUserList(userArray)}</h1>
-                
-                {/* <h1>Conversation between {conversation.user[0].username} and {conversation.user[1].username}</h1> */}
                 <div className="conversationContainer">
                     {generateMessageList(conversation.messages)}
                 </div>
-                
-                {/* <div>{generateMessageList(messageArray)} </div> */}
-                {/* {JSON.stringify(conversation, null, 2)} */}
-                {/* <h2>{conversation.user[0].name}</h2> */}
-                {/* <img src={conversation.avatar}></img> */}
             </div>
 
             <form onSubmit={(event) => handleSubmit(event)}>
                 <div className='input-field form-group'>
-					{/* <label htmlFor='content'>Content</label> */}
 					<input type='text'
 						name='content'
                         className='form-control messageBox'
@@ -157,7 +126,6 @@ const ConversationShow = () => {
 						value={content}
 					/>
 				</div>
-                {/* <input type='submit' value='Send Message' /> */}
                 <button type='submit' className='btn btn-primary'>Send Message</button>
             </form>
             <form onSubmit={(event) => handleDelete(event)}>
